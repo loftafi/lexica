1ca5502720425808dbda2868d87d4d4bf20a558b
loftafi <loftafi@proton.me>
2026-09-03 10:09:28 +1000

    Remove old ios template

--- ios/Lexica/main.cpp
+++
@@ -1,8 +0,0 @@
-//
-// Placeholder cpp file to declare SDL_MAIN_NEEDED
-//
-#include "main.hpp"
-#include <SDL3/SDL_main.h>
-#include <SDL3/SDL.h>
-#include <SDL3/SDL_render.h>
-#define SDL_MAIN_NEEDED
