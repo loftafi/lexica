779fdc412fd27e4bfa561c092af87513cea5e181
loftafi <loftafi@proton.me>
2026-09-03 11:06:36 +1000

    build.sh is executable

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ zig build ios \
     -Dapp_bundle=app_bundle.bd \
     -Dapp_owner=Jacob\ Rhoden \
     -Dorg=com.scripturial.dapp \
-    -Dapp_id=com.scripturial.bgd \
+    -Dapp_id=com.scripturial.bgd3 \
     -Dsplash_screen=assets-bgd/generated/splash-screen.jpg \
     -Dios_icon=assets-bgd/generated/app-icon-1024x1024.png
