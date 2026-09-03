82d4dc756f4aa4c2c595fac047d55741783935a2
loftafi <loftafi@proton.me>
2026-09-03 10:01:57 +1000

    Lexica build.sh

--- build.sh
+++ build.sh
@@ -1,15 +1,11 @@
 #!/bin/ksh
 
-#dialogos bundle
-
-zig build ios \
-    -Dapp_name=Biblical\ Greek \
-    -Ddev_mode=false \
-    -Dapp_bundle=app_bundle.bd \
-    -Dapp_owner=Jacob\ Rhoden \
-    -Dorg=com.scripturial.dapp \
-    -Dapp_id=com.scripturial.bgd \
-    -Dsplash_screen=assets-bgd/generated/splash-screen.jpg \
-    -Dios_icon=assets-bgd/generated/app-icon-1024x1024.png 
-
-
+ zig build ios \
+     -Dapp_name=Lexica \
+     -Ddev_mode=false \
+     -Dapp_bundle=app_bundle.bd \
+     -Dapp_owner=Lexica \
+     -Dorg=org.example.lexica \
+     -Dapp_id=org.example.lexica \
+     -Dsplash_screen=assets/generated/splash-screen.jpg \
+     -Dios_icon=assets/generated/app-icon-1024x1024.png
